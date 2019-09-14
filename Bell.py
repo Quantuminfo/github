3182859d680f9d7b82a9a72b87b3f8ef0d6ae387
Pedro Lauand <pedrolauand@Pedros-MacBook-Pro.local>
2019-09-14 18:40:51 -0300

    Código cenario 322 pronto.

--- Bell.py
+++ Bell.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -7,6 +8,11 @@ Created on Wed Jun  5 16:53:46 2019
 """
 
 import numpy as np
+from sympy.combinatorics import Permutation
+from sympy.combinatorics.perm_groups import PermutationGroup
+import copy
+
+
 ###cenário(k,m,n) k partes m perguntas e n respostas para cada pergunta
 ####4plets (abxy) perguntas x e y respostas a e b
 #(1111)-1:(2111)-2:(1121)-3:(2121)-4:(1211)-5:(2211)-6:(1221)-7:(2221)-8:(1112)-9
@@ -18,7 +24,6 @@ for i in range(partes):
     cenario[i]=cenario[i].split()
     for j in range(len(cenario[i])):
         cenario[i][j]=int( cenario[i][j])
-        
 alpha=input("alfa :")
 alpha=alpha.split()
 for i in range(len(alpha)):
@@ -26,6 +31,7 @@ for i in range(len(alpha)):
 #numero de perguntas len(cenario[j])
 #numero de respostas sum(cenario[j])
 listaB=[]
+projlistaB=[]
 for m in range(len(cenario)):  
 #construindo a base simetrica
     
@@ -86,15 +92,82 @@ for m in range(len(cenario)):
                             l[k].append(0)
     base=mhi+l+nhi
     listaB.append(base)
+    aux=copy.deepcopy(nhi)
 
-gamma,G=[],[]
-for i in listaB[1]:
-    for j in listaB[0]:
-        gamma.append(np.kron(i,j))
-for i in range(len(alpha)):
-    G.append(np.dot(gamma[i],alpha)/(np.linalg.norm(gamma[i]))**2)
-print(gamma[3],gamma[7],gamma[11],gamma[12],gamma[13],gamma[14],gamma[15])
+    for i in range(len(aux)):
+        for j in range(len(aux[i])):
+            aux[i][j]=0
+    projbase=mhi+l+aux
+    projlistaB.append(projbase)
+projauxiliar=[projlistaB[partes-1]]
+for m in range(partes-1):
+    projauxiliar1=[]
+    for i in projauxiliar[m]:
+        for j in projlistaB[partes-2-m]:
+            projauxiliar1.append(np.kron(i,j))
+    projauxiliar.append(projauxiliar1)
+projgamma=projauxiliar[partes-1] 
 
-             
+auxiliar=[listaB[partes-1]]
+for m in range(partes-1):
+    auxiliar1=[]
+    for i in auxiliar[m]:
+        for j in listaB[partes-2-m]:
+            auxiliar1.append(np.kron(i,j))
+    auxiliar.append(auxiliar1)
+gamma=auxiliar[partes-1]
+gamma=np.transpose(gamma)
+gamma=np.linalg.inv(gamma)
+Gamma=np.matmul(gamma,alpha)
+for i in range(len(gamma)):
+    if all(elem==0 for elem in projgamma[i]):
+        Gamma[i]=0
+beta=-Gamma[0]
+Gamma[0]=0
+print(Gamma,"<=",beta)
+
+list=[]
+for k in range(len(cenario)):
+    list.append(k)
+for k in range(len(cenario)):
+    list[k]=[]
+    for x in range(len(cenario[k])):
+        for a in range(cenario[k][x]):
+            list[k].append([a,x])
+print(list)
+  
+p1=Permutation(1, 4)(2, 8)(3, 12)(6, 9)(7, 13)(11, 14)
+p2=Permutation(0, 2)(1, 3)(4, 6)(5, 7)(8, 10)(9, 11)(12, 14)(13, 15)
+p3=Permutation(0, 8)(1, 9)(2, 10)(3, 11)(4, 12)(5, 13)(6, 14)(7, 15)
+p4=Permutation(0, 1)(4, 5)(8, 9)(12, 13)
+p5=Permutation(2, 3)(6, 7)(10, 11)(14, 15)
+p6=Permutation(0, 4)(1, 5)(2, 6)(3, 7)
+p7=Permutation(8, 12)(9, 13)(10, 14)(11, 15)
+G=PermutationGroup(p1,p2,p3,p4,p5,p6,p7)
+lista=[]
+Stab=[G]
+for j in range(len(alpha)):
+    lista.append(j)
+    Stab.append(G.pointwise_stabilizer(lista))
     
-
+U=[]
+for i in range(len(Stab)-1):
+    U.append(Stab[i].coset_transversal(Stab[i+1]))
+H=[[G.identity]]
+for i in range(len(Gamma)-1):
+    H.append([])
+for j in range(len(Gamma)-1):
+    m=np.infty
+    for u in U[j]:
+        for h in H[j]:
+            perm=[i^u^h for i in range(len(Gamma))]
+            if Gamma[perm[j]]<m:
+                m=Gamma[perm[j]]
+                H[j+1]=[]
+            if Gamma[perm[j]]==m:
+                H[j+1].append(u*h)
+permu=[i^H[len(Gamma)-1][0] for i in range(len(Gamma))]
+aux1=copy.deepcopy(Gamma)
+for i in range(len(Gamma)):
+    alpha[i]=aux1[permu[i]]
+print(alpha)
